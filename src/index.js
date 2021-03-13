624b7dd9545b79a0667fb0a9a389b9c1f40dca47
Matheus Bandeira <mbandeira.cs@gmail.com>
2021-03-13 12:10:46 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,19 +10,54 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) return response.status(404).json({ error: "user doesn't exist" })
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (!user.pro & user.todos.length >= 10) return response.status(403).json("reached todos limit (10), upgrade to pro to continue creating")
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+  if (!user) return response.status(404).json({ error: "user doesn't exist" })
+
+  const regexUuid = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+  if (!regexUuid.test(id)) return response.status(400).json({ error: "id isn't valid" })
+
+  const todo = user.todos.find(todo => todo.id == id)
+  if (!todo) return response.status(404).json({ error: `todo doesn't exist` });
+
+  request.user= user;
+  request.todo= todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if (!user) return response.status(404).json({ error: "user doesn't exist" })
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
